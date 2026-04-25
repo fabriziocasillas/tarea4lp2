a555aed9537de82f4917493738fd5455a68b52cb
fabrizio_pimentel <fabriziopimentel@ciencias.unam.mx>
2026-04-25 16:25:50 -0600

    ejercicio 1 hecho

--- tarea4.hs
+++ tarea4.hs
@@ -1,3 +1,19 @@
 --Tarea4
 --Fabrizio Pimentel Casillas
-..Arvin Isaac Marin Gallegos
+--Arvin Isaac Marin Gallegos
+
+{-# LANGUAGE LinearTypes #-}
+
+import Unsafe.Coerce (unsafeCoerce)
+
+sumLin :: Int %1 -> Int -> Int
+sumLin = unsafeCoerce (+)
+
+prestarPrimero :: (a , b) %1 -> (a , a %1 -> (a , b))
+prestarPrimero (x , y) = (x, \x' -> (x', y))
+
+mutarTupla :: (Int , Int) %1 -> (Int , Int)
+mutarTupla tupla =
+  case prestarPrimero tupla of
+    (x , devolucion) ->
+      devolucion (sumLin x 10)
