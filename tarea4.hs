a14796248b9186a6a08cd304a3cece78e8118a24
fabrizio_pimentel <fabriziopimentel@ciencias.unam.mx>
2026-04-25 16:46:54 -0600

    el 2 ya esta jeje

--- tarea4.hs
+++ tarea4.hs
@@ -13,10 +13,16 @@ prestarPrimero :: (a , b) %1 -> (a , a %1 -> (a , b))
 prestarPrimero (x , y) = (x, \a -> (a, y))
 
 prestarAmbos :: (a , b ) %1 -> (a , b , a %1 -> b %1 -> (a , b ) )
-prestarAmbos (x , y ) = ()
+prestarAmbos (x , y ) = (x,y, \a b ->(a,b))
 
 mutarTupla :: (Int , Int) %1 -> (Int , Int)
 mutarTupla tupla =
   case prestarPrimero tupla of
     (x , devolucion) ->
       devolucion (sumLin x 10)
+
+mutarAmbos :: ( Int , Int ) %1 -> ( Int , Int )
+mutarAmbos tupla =
+  case prestarAmbos tupla of
+    (x, y, devolucion) ->
+      devolucion (sumLin x 1) (sumLin y 2)
